365c0069ce471ed7c86395661322802a057dd238
Lebot Apps <lebot.llc@gmail.com>
2021-06-01 18:56:46 -0400

    REVISED--------Greeting express application created.

--- server.js
+++ server.js
@@ -12,11 +12,20 @@ app.get(`/greeting`, (req, res) => {
     res.send("Hello, stranger");
 });
 
+//route to Jimmy-boy
+
+app.get('/greeting/Jimmy-boy', (req, res) => {
+	console.log(req.params);
+	res.send(`Wow! Hello there, Jimmy-boy`);
+});
+
 //route to param /:name
 
 app.get('/greeting/:name', (req, res) => {
 	console.log(req.params);
 	res.send('Whats Up ' + req.params.name + ` ! ` + ` It's so great to see you!`);
 });
+
+
 
 app.listen(3000);
