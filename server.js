5b8a04f27f780e32d0358b6c532c1fa6a90a7b7e
Lebot Apps <lebot.llc@gmail.com>
2021-06-02 00:12:10 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -30,9 +30,32 @@ app.get('/greeting/:name', (req, res) => {
 
 app.get('/tip/:total/:tipPercentage', (req, res) => {
     
+    parseInt = "100";
+    parseInt = "20";
+
     console.log(req.params);
     res.send(req.params.tipPercentage);
-
 });
+
+//Magic Ball route answers set to variable
+
+const ballAnswers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
+//index a random answer 
+
+const randomAnswer = ballAnswers[Math.floor(Math.random() * ballAnswers.length)];
+
+//Link HTML file to Express 
+
+app.get('/magic', function(req, res) {
+    res.sendFile('index.html', {root: __dirname })
+});
+
+//Return Question and Answer using HTML 
+
+app.get('/myform', function(req, res){  
+    var myText = req.query.mytext; //mytext is the name of your input box 
+    res.send( `<h1> Your Queston: ${myText} Your Answer: ${randomAnswer} </h1>`);  
+});  
 
 app.listen(3000);
